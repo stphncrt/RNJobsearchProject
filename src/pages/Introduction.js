258873948f69a37772e56a2eed00779f97138c3e
stphncrt <cretestephen@gmail.com>
2021-03-18 01:15:51 +0300

    topic Item added

--- src/pages/Introduction.js
+++ src/pages/Introduction.js
@@ -1,10 +1,64 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, ScrollView} from 'react-native';
 
+import {TopicItem} from '../components/';
+
+const topic = [
+  {
+    id: 0,
+    name: 'Java',
+    color: 'fb5607',
+  },
+  {
+    id: 1,
+    name: 'Pyhon',
+    color: '007f5f',
+  },
+  {
+    id: 2,
+    name: 'JavaScript',
+    color: 'ffb703',
+  },
+  {
+    id: 3,
+    name: '.NET',
+    color: '023e7d',
+  },
+  {
+    id: 4,
+    name: 'Dart',
+    color: '009688',
+  },
+  {
+    id: 5,
+    name: 'Go',
+    color: 'fb5607',
+  },
+  {
+    id: 6,
+    name: 'Ruby',
+    color: '00bcd4',
+  },
+  {
+    id: 7,
+    name: 'C',
+    color: 'ff9800',
+  },
+  {
+    id: 8,
+    name: 'C++',
+    color: 'c0ca33',
+  },
+];
 const Introduction = props => {
   return (
     <View>
-      <Text>Inrroductio</Text>
+      <Text>Introduction</Text>
+      <ScrollView>
+        {topic.map(topic => (
+          <TopicItem key={topic.id} item={topic} />
+        ))}
+      </ScrollView>
     </View>
   );
 };
---
+++ src/styles.js
@@ -0,0 +1,15 @@
+import {StyleSheet} from 'react-native';
+
+export const topicItem = StyleSheet.create({
+  container: {
+    // backgroundColor:
+    padding: 8,
+    borderRadius: 6,
+    margin: 5,
+  },
+  text: {
+    fontWeight: 'bold',
+    color: 'white',
+    fontSize: 20,
+  },
+});
